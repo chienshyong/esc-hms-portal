8149c1abfd99308dccc1a3cce7d0c836f58a6c91
DheepthaRajesh <123943688+DheepthaRajesh@users.noreply.github.com>
2023-07-20 20:02:14 +0800

    Added navbar to landlord timeline

--- frontend/app/Landlord/timeline/page.js
+++ frontend/app/Landlord/timeline/page.js
@@ -1,17 +1,15 @@
 'use client'
 import { Typography, Grid } from '@mui/material';
 import Timeline from './timeline';
+import Navbar from '../navbar'
 
 export default function LandlordPage() {
   return (
     <body style={{backgroundColor: "#F5F5F5"}}>
-      <Grid container style={{width:"100vw",height:"100vh"}}>
-        <Grid item xs={2} 
-        style={{backgroundColor: "#ff8a80" }}
-        >
-          Navbar
-        </Grid>
-        <Grid item={true} xs={10} direction="column" style={{ padding: '20px' }}>
+      <Navbar></Navbar>
+      <Grid container style={{width:"100vw",height:"100vh",justifyContent:"center",alignItems:"center"}}>
+      <Grid item xs={2}></Grid>
+        <Grid item container xs={9} direction="column">
             {/* Can change this to retrieve from the list of cases later: */}
           <Typography variant="h4" fontWeight="bold" align='center' margin="2ch 0 1ch 0">Cleanliness</Typography>
           <Typography variant="overline" align='center' display="block" gutterBottom>
@@ -19,6 +17,7 @@ export default function LandlordPage() {
             </Typography>
           <Timeline/>
         </Grid>
+        <Grid item xs={2}></Grid>
       </Grid>
     </body>
   );

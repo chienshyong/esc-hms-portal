4ac75899db143a0bae05c95fee0ae0729aa4fd3b
Miguel Ong (@ahalation) <agitatehalation@gmail.com>
2023-07-23 15:30:39 +0800

    renamed dir folders

---
+++ frontend/app/tenant/page.test.js
@@ -0,0 +1,17 @@
+const TenantOptions = require('../page.js');
+
+
+describe('pages', () => {
+ 
+  test('test for page.js could be test', () => {
+
+    
+        // Wrap the function call in a function to catch the error and then throw it
+      //   expect(() => {
+      //     // if input list is empty, there will be an error
+      //     TenantOptions();
+      // }).toThrowError('Test error');
+
+         
+  })
+});

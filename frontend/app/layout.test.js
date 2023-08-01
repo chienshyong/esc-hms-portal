e4d2e1dd8b9d50bfd0cf8bc56bd31570423887a1
K15H3N <kishend03@gmail.com>
2023-08-01 22:25:31 +0800

    frontend test fixes
    layout.test.js passes

--- frontend/app/layout.test.js
+++ frontend/app/layout.test.js
@@ -1,8 +1,17 @@
+import 'cross-fetch/polyfill';
 import { render, screen } from '@testing-library/react';
 import RootLayout from './layout';
 
+// Mock the 'next/font/google' module and the 'Inter' function
+jest.mock('next/font/google', () => ({
+  Inter: jest.fn().mockReturnValue({ className: 'mocked-inter-class' }),
+}));
+
 describe('RootLayout', () => {
   it('renders the root layout component correctly', () => {
+    // Mock the fetch function with an absolute URL
+    global.fetch = jest.fn().mockResolvedValue({});
+
     render(<RootLayout />);
     
 

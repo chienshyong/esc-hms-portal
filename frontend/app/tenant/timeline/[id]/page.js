03d680da156db0d0aa92cbefcc9b876db77f5e48
DheepthaRajesh <dheeptha_rajesh@mymail.sutd.edu.sg>
2023-08-04 11:28:19 +0800

    Migrated timeline

--- frontend/app/tenant/timeline/[id]/page.js
+++ frontend/app/tenant/timeline/[id]/page.js
@@ -1,11 +1,25 @@
 'use client'
-import { tenantauth } from "@/app/auth"
+import { Typography, Grid } from '@mui/material';
+import Timeline from './timeline';
+import Navbar from '../navbar';
 
-export default function Timeline({params}) {
-    tenantauth()
-    return (
-        <section>
-            <h1 className="mb-1 text-4xl font-extrabold text-center">Viewing Timeline: {params.id}</h1>
-        </section>
-    )
+export default function TenantTimeline() {
+  return (
+    <body style={{backgroundColor: "#F5F5F5"}}>
+      <Navbar></Navbar>
+      <Grid container style={{width:"100vw",height:"100vh",justifyContent:"center",alignItems:"center"}}>
+      <Grid item xs={2}></Grid>
+        <Grid item container xs={9} direction="column">
+            {/* Can change this to retrieve from the list of cases later: */}
+          <Typography variant="h4" fontWeight="bold" align='center' margin="2ch 0 1ch 0">Cleanliness</Typography>
+
+          <Typography variant="overline" align='center' display="block" gutterBottom>
+            ServiceRequestID: SR00001
+            </Typography>
+          <Timeline/>
+        </Grid>
+        <Grid item xs={2}></Grid>
+      </Grid>
+    </body>
+  );
 }

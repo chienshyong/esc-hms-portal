3b2752e17c13d1dae0d0e54f73489d015e77a004
DheepthaRajesh <123943688+DheepthaRajesh@users.noreply.github.com>
2023-08-06 01:04:43 +0800

    Migrated timeline

---
+++ frontend/app/landlord/timeline/[id]/page_withoutAuth.js
@@ -0,0 +1,26 @@
+'use client'
+import { Typography, Grid } from '@mui/material';
+import Timeline from './timeline';
+import Navbar from '../navbar'
+
+export default function LandlordPage() {
+  return (
+    <body style={{backgroundColor: "#F5F5F5"}}>
+      <Navbar></Navbar>
+      <Grid container style={{width:"100vw",height:"100vh",justifyContent:"center",alignItems:"center"}}>
+      <Grid item xs={2}></Grid>
+        <Grid item container xs={9} direction="column">
+            {/* Can change this to retrieve from the list of cases later: */}
+          <Typography variant="h4" fontWeight="bold" align='center' margin="2ch 0 1ch 0">Cleanliness</Typography>
+          <Typography variant="overline" align='center' display="block" gutterBottom>
+            ServiceRequestID: SR00001
+            </Typography>
+          <Timeline/>
+        </Grid>
+        <Grid item xs={2}></Grid>
+      </Grid>
+    </body>
+  );
+}
+
+
